caa0aaf57f43cde702eb24cd5b177e9fe1edfb7f
AjaykumarGV <102965732+AjaykumarGV@users.noreply.github.com>
2022-10-28 14:36:26 +0530

    Initial Commit1

---
+++ AjayKumarDetails.java
@@ -0,0 +1,55 @@
+package org.ajay;
+
+public class AjayKumarDetails {
+
+	private void ajayKumarNumber() {
+		System.out.println("Ajay Kumar Number is 9524733145");
+	}
+
+	private void ajayKumarInstaId() {
+		System.out.println("Ajay Kumar Insta Id is mr.ajay_aug10");
+	}
+
+	private void ajayKumarAddress() {
+		System.out.println("Ajay Kumar Address is Chennai");
+
+	}
+
+	private void ajayKumarFatherName() {
+		System.out.println("Ajay Kumar Father Name is Ganapathy");
+
+	}
+
+	private void ajayKumarMotherName() {
+		System.out.println("Ajay Kumar Mother Name is Venkateshwari");
+	}
+
+	private void ajayKumarSisterName() {
+		System.out.println("Ajay Kumar Sister Name is Vidhya");
+
+	}
+
+	private void ajayKumarLifePartnerName() {
+		System.out.println("Ajay Kumar LifePartner Name is Sasi");
+
+	}
+
+	private void ajayLifeGoal() {
+		System.out.println("Become a Business Man");
+
+	}
+
+	public static void main(String[] args) {
+		AjayKumarDetails a = new AjayKumarDetails();
+		a.ajayKumarNumber();
+		a.ajayKumarInstaId();
+		a.ajayKumarAddress();
+		a.ajayKumarFatherName();
+		a.ajayKumarMotherName();
+		a.ajayKumarSisterName();
+		a.ajayKumarLifePartnerName();
+		a.ajayLifeGoal();
+
+	}
+
+}
